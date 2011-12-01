dcd9c9d5a1f78431ed55381b386b838462be7b14
Rustam Ganeyev <rganeyev@gmail.com>
2011-12-01 05:00:10 +0400

    added unit file to test replace feature

---
+++ MonoFluentStringTests/FluentStrings.Tests/ReplaceTests.cs
@@ -0,0 +1,16 @@
+using System;
+using NUnit.Framework;
+
+namespace FluentStrings.Tests
+{
+    [TestFixture()]
+	public class ReplaceTests
+    {
+        //TODO: write tests here
+        [Test()]
+        public void TestCase ()
+        {
+        }
+    }
+}
+
