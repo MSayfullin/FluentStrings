5415f19e2de5f29309096fdc72e38f0192976d71
Marsel Sayfullin <M.Sayfullin@gmail.com>
2013-02-10 00:02:50 +0400

    Refactor vowel maps (move to dedicated static class)

--- FluentStrings/Actions/Remove/Auxiliary/RemoveVowels.cs
+++ FluentStrings/Actions/Remove/Auxiliary/RemoveVowels.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
+using dokas.FluentStrings.Actions.Remove.Resources;
 
 namespace dokas.FluentStrings.Actions.Remove
 {
@@ -19,26 +19,7 @@ namespace dokas.FluentStrings.Actions.Remove
 
         public override string ToString()
         {
-            char[] vowels;
-            switch (Thread.CurrentThread.CurrentCulture.Name)
-            {
-                case "en":
-                case "en-US":
-                case "en-CA":
-                case "en-GB":
-                case "en-NZ":
-                case "en-IE":
-                case "en-AU":
-                    vowels = new char[] { 'a', 'e', 'i', 'o', 'u' };
-                    break;
-                case "ru":
-                case "ru-RU":
-                    vowels = new char[] { 'а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е' };
-                    break;
-                default:
-                    vowels = new char[0];
-                    break;
-            }
+            var vowels = VowelsMap.GetFor(Thread.CurrentThread.CurrentCulture.Name);
             return _source.RemoveChars(vowels).IgnoringCase();
         }
     }
---
+++ FluentStrings/Actions/Remove/Resources/VowelsMap.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace dokas.FluentStrings.Actions.Remove.Resources
+{
+    internal static class VowelsMap
+    {
+        public static char[] GetFor(string cultureName)
+        {
+            switch (cultureName)
+            {
+                case "en":
+                case "en-US":
+                case "en-CA":
+                case "en-GB":
+                case "en-NZ":
+                case "en-IE":
+                case "en-AU":
+                    return new char[] { 'a', 'e', 'i', 'o', 'u' };
+                case "ru":
+                case "ru-RU":
+                    return new char[] { 'а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е' };
+                default:
+                    return new char[] { };
+            }
+        }
+    }
+}
