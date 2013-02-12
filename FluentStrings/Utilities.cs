487759b97c9a0cdd6ac3a1253949173472c89742
Marsel Sayfullin <M.Sayfullin@gmail.com>
2013-02-13 00:26:07 +0400

    Add Utilities actions
    .IsEmpty and .Format are implemented

---
+++ FluentStrings/Utilities.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace dokas.FluentStrings
+{
+    public static class Utilities
+    {
+        /// <summary>
+        /// Checks whether string is empty or not
+        /// N.B.: Equivalent of String.IsNullOrEmpty method
+        /// </summary>
+        /// <param name="source">Targets string for examination</param>
+        /// <returns>True if string is null or empty, otherwise false</returns>
+        public static bool IsEmpty(this string source)
+        {
+            // TODO: Use String.IsNullOrWhiteSpace() when switched to .NET 4.0
+            return String.IsNullOrEmpty(source);
+        }
+
+        /// <summary>
+        /// Replaces the format item in a template with the text equivalent
+        /// of the value specified in an array.
+        /// </summary>
+        public static string Format(this string template, params object[] values)
+        {
+            return String.Format(template, values);
+        }
+    }
+}
