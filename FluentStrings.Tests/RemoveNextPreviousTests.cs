6b62c7296582015a3f634e24825e087aa8d8a865
Marsel Sayfullin <M.Sayfullin@gmail.com>
2013-02-16 00:15:50 +0400

    Move RemoveNextPrevious actions' tests to a separate file

---
+++ FluentStrings.Tests/RemoveNextPreviousTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace dokas.FluentStrings.Tests
+{
+    [TestClass]
+    public class RemoveNextPreviousTests
+    {
+        #region Remove Starting Next
+
+        [TestMethod]
+        public void RemoveNext()
+        {
+            string transformed = "Some very long string".Remove().Starting(1).Next(3);
+            transformed.Should().Be("");
+        }
+
+        #endregion
+
+        #region Remove Starting From Next
+
+        [TestMethod]
+        public void RemoveFromNext()
+        {
+            string transformed = "Some very long string".Remove().Starting(1).From(The.Beginning).Next(3);
+            transformed.Should().Be("");
+        }
+
+        #endregion
+
+        #region Remove Starting Previous
+
+        [TestMethod]
+        public void RemovePrevious()
+        {
+            string transformed = "Some very long string".Remove().Starting(1).Previous(3);
+            transformed.Should().Be("");
+        }
+
+        #endregion
+
+        #region Remove Starting From Previous
+
+        [TestMethod]
+        public void RemoveFromPrevious()
+        {
+            string transformed = "Some very long string".Remove().Starting(1).From(The.Beginning).Previous(3);
+            transformed.Should().Be("");
+        }
+
+        #endregion
+    }
+}
--- FluentStrings.Tests/RemoveTests.cs
+++ FluentStrings.Tests/RemoveTests.cs
@@ -159,49 +159,5 @@ namespace dokas.FluentStrings.Tests
         }
 
         #endregion
-
-        #region Remove Starting Next
-
-        [TestMethod]
-        public void RemoveNext()
-        {
-            string transformed = "Some very long string".Remove().Starting(1).Next(3);
-            transformed.Should().Be("");
-        }
-
-        #endregion
-
-        #region Remove Starting From Next
-
-        [TestMethod]
-        public void RemoveFromNext()
-        {
-            string transformed = "Some very long string".Remove().Starting(1).From(The.Beginning).Next(3);
-            transformed.Should().Be("");
-        }
-
-        #endregion
-
-        #region Remove Starting Previous
-
-        [TestMethod]
-        public void RemovePrevious()
-        {
-            string transformed = "Some very long string".Remove().Starting(1).Previous(3);
-            transformed.Should().Be("");
-        }
-
-        #endregion
-
-        #region Remove Starting From Previous
-
-        [TestMethod]
-        public void RemoveFromPrevious()
-        {
-            string transformed = "Some very long string".Remove().Starting(1).From(The.Beginning).Previous(3);
-            transformed.Should().Be("");
-        }
-
-        #endregion
     }
 }
