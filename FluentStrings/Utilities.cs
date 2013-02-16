929cedfa75f1bd65e0e87ea33fa346a1a59fbb20
Marsel Sayfullin <M.Sayfullin@gmail.com>
2013-02-16 14:13:19 +0400

    Extend and correct comments

--- FluentStrings/Utilities.cs
+++ FluentStrings/Utilities.cs
@@ -8,7 +8,7 @@ namespace dokas.FluentStrings
         /// Checks whether string is empty or not
         /// N.B.: Equivalent of String.IsNullOrEmpty method
         /// </summary>
-        /// <param name="source">Targets string for examination</param>
+        /// <param name="source">Target string for examination</param>
         /// <returns>True if string is null or empty, otherwise false</returns>
         public static bool IsEmpty(this string source)
         {
@@ -18,8 +18,10 @@ namespace dokas.FluentStrings
 
         /// <summary>
         /// Replaces the format item in a template with the text equivalent
-        /// of the value specified in an array.
+        /// of the value specified in a values array.
         /// </summary>
+        /// <param name="template">Formating template</param>
+        /// <param name="values">Values for fomating template</param>
         public static string Format(this string template, params object[] values)
         {
             return String.Format(template, values);
