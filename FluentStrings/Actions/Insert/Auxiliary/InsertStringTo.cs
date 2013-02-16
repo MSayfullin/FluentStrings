5c57e6ba3ed51ba0ad4c5b9b62f7fee8894202cf
Marsel Sayfullin <M.Sayfullin@gmail.com>
2013-02-17 01:35:35 +0400

    Enhance position handling on Insert.To action
    Not supported StartOf and EndOf values was handled

--- FluentStrings/Actions/Insert/Auxiliary/InsertStringTo.cs
+++ FluentStrings/Actions/Insert/Auxiliary/InsertStringTo.cs
@@ -1,4 +1,6 @@
-﻿namespace dokas.FluentStrings.Actions.Insert
+﻿using System;
+
+namespace dokas.FluentStrings.Actions.Insert
 {
     public class InsertStringTo
     {
@@ -18,11 +20,19 @@
 
         public override string ToString()
         {
-            return _position == The.Beginning
-                ? _insertString
-                : (_insertString.Source == null && _insertString.Insertion == null)
-                    ? null
-                    : _insertString.Source + _insertString.Insertion;
+            switch (_position)
+            {
+                case The.Beginning:
+                    return _insertString;
+                case The.End:
+                    return (_insertString.Source == null && _insertString.Insertion == null)
+                        ? null
+                        : _insertString.Source + _insertString.Insertion;
+                case The.StartOf:
+                case The.EndOf:
+                default:
+                    throw new ArgumentOutOfRangeException("position", "Only Beginning and End positions are supported by Insert().To() method");
+            }
         }
     }
 }
