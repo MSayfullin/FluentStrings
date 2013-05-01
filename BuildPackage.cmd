7fb39876202d5f4df0c97f474440c7e7c7887ea3
Marsel Sayfullin <M.Sayfullin@gmail.com>
2013-05-01 23:11:50 +0400

    Add portable library version
    Move bin for release build configuration to common place and separate diffrent versions by folder

--- BuildPackage.cmd
+++ BuildPackage.cmd
@@ -1,4 +1,4 @@
 @ECHO OFF
-SET PACKAGESPATH=FluentStrings\bin\Packages
+SET PACKAGESPATH=bin\packages
 IF NOT EXIST %PACKAGESPATH% md %PACKAGESPATH%
 "%VS110COMNTOOLS%nuget" pack dokas.FluentStrings.nuspec -OutputDirectory %PACKAGESPATH% -Symbols -Version %1
--- FluentStrings.Portable/Properties/AssemblyInfo.cs
+++ FluentStrings.Portable/Properties/AssemblyInfo.cs
@@ -3,6 +3,5 @@
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Fluent Strings (3.5 Client Profile)")]
+[assembly: AssemblyTitle("Fluent Strings (Portable)")]
 [assembly: AssemblyCulture("")]
-
