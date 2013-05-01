117a46812389544974b13ada272ba8c3e2a81c30
Marsel Sayfullin <M.Sayfullin@gmail.com>
2013-05-01 23:22:49 +0400

    Add compilation step to build batch
    Add NuGet into repository itself and re-reference it

--- BuildPackage.cmd
+++ BuildPackage.cmd
@@ -1,4 +1,5 @@
 @ECHO OFF
 SET PACKAGESPATH=bin\packages
 IF NOT EXIST %PACKAGESPATH% md %PACKAGESPATH%
-"%VS110COMNTOOLS%nuget" pack dokas.FluentStrings.nuspec -OutputDirectory %PACKAGESPATH% -Symbols -Version %1
+"%WINDIR%\Microsoft.NET\Framework\v4.0.30319\MSBuild" FluentStrings.sln /t:Rebuild /p:Configuration=Release /m
+packages\NuGet pack dokas.FluentStrings.nuspec -OutputDirectory %PACKAGESPATH% -Symbols -Version %1
